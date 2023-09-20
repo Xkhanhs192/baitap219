d03812a162f3482ebfc7b52cc6b2de3a499a67f4
Xkhanhs192 <145523014+Xkhanhs192@users.noreply.github.com>
2023-09-21 02:07:13 +0700

    Add files via upload

---
+++ Baitap4.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baitap4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Baitap4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("a     a^2    a^3    a^4");
+       System.out.println("1     1      1      1 ");
+       System.out.println("2     4      8      16");
+       System.out.println("3     9      27     81");
+       System.out.println("4     16     64    256");
+    }
+    
+}
